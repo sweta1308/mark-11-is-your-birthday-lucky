b8c9354c450a6bff8ed397b33f7929cbb9695eb2
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2022-08-29 16:43:27 +0530

    feat: updated project

---
+++ index.js
@@ -0,0 +1,33 @@
+const dateOfBirth = document.querySelector("#dob");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkBtn = document.querySelector(".btn-check");
+const result = document.querySelector(".output");
+
+checkBtn.addEventListener('click', checkDobIsLucky);
+
+function checkDobIsLucky() {
+    const birthDate = dateOfBirth.value;
+    const sum = calculateSum(birthDate);
+    if (sum&&birthDate)
+    compareValues(sum, luckyNumber.value);
+    else result.innerText = "Please enter both the fields 🤔";
+
+}
+
+function calculateSum(birthDate) {
+    birthDate = birthDate.replaceAll("-", "");
+    let sum = 0;
+    for (i = 0; i < birthDate.length; i++) {
+        sum = sum + Number(birthDate.charAt(i));
+    }
+    return sum;
+}
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        result.innerText = "Yayy! Your Birthday is lucky 🤩";
+    } else {
+        result.innerText = "Oops! Your birthday is not lucky 😢";
+    }
+}
+
